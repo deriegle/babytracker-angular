d83796d56c724b4e908218e96b55f894de35e0d5
Devin Chase Riegle <devin@localhost.localdomain>
2018-01-04 00:38:24 -0500

    Trying to get collapsible to work

--- src/app/components/feedings/feedings.component.ts
+++ src/app/components/feedings/feedings.component.ts
@@ -9,13 +9,29 @@ import { Feeding } from '../../models/feeding';
 })
 export class FeedingsComponent implements OnInit {
   feedings: Feeding[];
-
+  months: string[] = [];
   constructor(private feedingService: FeedingService) { }
 
   ngOnInit() {
     this.feedingService.getFeedings().subscribe(feedings => {
       //console.log(feedings);
       this.feedings = feedings;
+      this.getMonths()
+    });
+  }
+
+  getMonths(): any {
+    var monthStrings = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.feedings.forEach((feed) => {
+      var date = new Date(feed.date);
+      var month = date.getMonth();
+      var year = date.getFullYear();
+      var string = new Date(monthStrings[month] + ' ' + year).toISOString();
+      //var string = monthStrings[month] + ' ' + year;
+      if(this.months.indexOf(string) === -1) {
+        this.months.push(string);
+      }
+      console.log(this.months);
     });
   }
 
