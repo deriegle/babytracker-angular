2d44920af371a452b1b900cd0d5dcfe2bdd4f34d
Devin Chase Riegle <devin@localhost.localdomain>
2018-01-05 23:35:20 -0500

    Added Delete Functionality to Feedings Component

--- README.md
+++ README.md
@@ -1,27 +1,19 @@
 # BabyTracker
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.3.
+
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
 
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+## Todo
+1. Remove Month from Baby Tracker homepage after deleting all feeding in that month
+2. When adding feeding, add it to the current Firestore document for that month if available
+3. Get checkboxes working on Add-Feeding component
+4. Add easier delete buttons (currently you dblclick on time)
+5. Update functionality
+6. Authentication/Login & Register pages
--- src/app/components/feedings/feedings.component.ts
+++ src/app/components/feedings/feedings.component.ts
@@ -10,14 +10,18 @@ import { Feeding } from '../../models/feeding';
 export class FeedingsComponent implements OnInit {
   feedings: Feeding[];
   months: string[] = [];
+  editState: boolean = false;
+  feedingToEdit: Feeding;
+
   constructor(private feedingService: FeedingService) { }
 
   ngOnInit() {
+    /* Subscribe to feedings to get any updates/deletes, etc */
     this.feedingService.getFeedings().subscribe(feedings => {
-      //console.log(feedings);
+
       this.feedings = feedings;
+      /* Get all the months for the feedings in the database & sort them from most recent first */
       this.getMonths()
-      // Sort Months by ISO Format after getting them
       this.months = this.sortMonthsDesc(this.months);
     });
   }
@@ -26,8 +30,15 @@ export class FeedingsComponent implements OnInit {
     this.feedingService.deleteFeeding(feeding);
   }
 
+  editFeeding(e, feeding){
+    this.editState = true;
+    this.feedingToEdit = feeding;
+  }
+
   getMonths(): any {
     var monthStrings = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    /* Grab the month & year for each feeding in the database,
+    Add each month to the array as long as it hasn't been added previously */
     this.feedings.forEach((feed) => {
       var date = new Date(feed.date);
       var month = date.getMonth();
@@ -41,6 +52,7 @@ export class FeedingsComponent implements OnInit {
     });
   }
 
+  /* Use ISO format to sort Months in Database from Newest to Oldest */
   sortMonthsDesc(arr){
     arr = arr.sort(function(a, b){
       return (a < b) ? 1 : ((a > b) ? -1 : 0);
@@ -48,6 +60,7 @@ export class FeedingsComponent implements OnInit {
     return arr;
   }
 
+  /* Use ISO format to sort Months in Databse from Oldest to Newest */
   sortMonthsAsc(arr){
     arr = arr.sort(function(a, b){
       return (a < b) ? -1 : ((a > b) ? 1 : 0);
